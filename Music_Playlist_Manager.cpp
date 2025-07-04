57f856c5efd5b15f5040f11df52633a6cc3495a9
Rohit Chand <119827556+The-Kind-Sage@users.noreply.github.com>
2025-07-04 23:42:54 +0545

    Update Music_Playlist_Manager.cpp

--- Music_Playlist_Manager.cpp
+++ Music_Playlist_Manager.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -9,21 +13,39 @@ struct Song {
     Song* prev;
     Song* next;
 
-    Song(string t) : title(t), prev(nullptr), next(nullptr) {}
+    Song(const string& t) : title(t), prev(nullptr), next(nullptr) {}
 };
 
-// Playlist class
 class Playlist {
 private:
     Song* head;
     Song* tail;
     Song* current;
+    int size;
+
+    // Helper to find a song by title
+    Song* findSong(const string& title) {
+        Song* temp = head;
+        while (temp) {
+            if (temp->title == title) return temp;
+            temp = temp->next;
+        }
+        return nullptr;
+    }
 
 public:
-    Playlist() : head(nullptr), tail(nullptr), current(nullptr) {}
+    Playlist() : head(nullptr), tail(nullptr), current(nullptr), size(0) {}
 
-    // Add song to the playlist
-    void addSong(string title) {
+    ~Playlist() {
+        clear();
+    }
+
+    // Add song to the playlist (no duplicates)
+    void addSong(const string& title) {
+        if (findSong(title)) {
+            cout << "Song \"" << title << "\" already exists in the playlist.\n";
+            return;
+        }
         Song* newSong = new Song(title);
         if (!head) {
             head = tail = current = newSong;
@@ -32,48 +54,59 @@ public:
             newSong->prev = tail;
             tail = newSong;
         }
+        ++size;
         cout << "Added: " << title << endl;
     }
 
     // Remove song from the playlist
-    void removeSong(string title) {
-        Song* temp = head;
-        while (temp) {
-            if (temp->title == title) {
-                if (temp->prev) temp->prev->next = temp->next;
-                if (temp->next) temp->next->prev = temp->prev;
-                if (temp == head) head = temp->next;
-                if (temp == tail) tail = temp->prev;
-                if (temp == current) current = temp->next ? temp->next : temp->prev;
-                delete temp;
-                cout << "Removed: " << title << endl;
-                return;
-            }
-            temp = temp->next;
+    void removeSong(const string& title) {
+        Song* temp = findSong(title);
+        if (!temp) {
+            cout << "Song not found!\n";
+            return;
         }
-        cout << "Song not found!\n";
+        if (temp->prev) temp->prev->next = temp->next;
+        if (temp->next) temp->next->prev = temp->prev;
+        if (temp == head) head = temp->next;
+        if (temp == tail) tail = temp->prev;
+        if (temp == current) current = temp->next ? temp->next : temp->prev;
+        delete temp;
+        --size;
+        cout << "Removed: " << title << endl;
     }
 
     // Display playlist forward
-    void displayForward() {
+    void displayForward() const {
         Song* temp = head;
         cout << "\nPlaylist (Forward):\n";
+        if (!temp) {
+            cout << "[Empty]\n";
+            return;
+        }
         while (temp) {
-            cout << temp->title << " -> ";
+            if (temp == current) cout << ">> ";
+            cout << temp->title;
+            if (temp->next) cout << " -> ";
             temp = temp->next;
         }
-        cout << "END\n";
+        cout << "\n";
     }
 
     // Display playlist backward
-    void displayBackward() {
+    void displayBackward() const {
         Song* temp = tail;
         cout << "\nPlaylist (Backward):\n";
+        if (!temp) {
+            cout << "[Empty]\n";
+            return;
+        }
         while (temp) {
-            cout << temp->title << " -> ";
+            if (temp == current) cout << ">> ";
+            cout << temp->title;
+            if (temp->prev) cout << " -> ";
             temp = temp->prev;
         }
-        cout << "START\n";
+        cout << "\n";
     }
 
     // Play next song
@@ -81,8 +114,10 @@ public:
         if (current && current->next) {
             current = current->next;
             cout << "Now playing: " << current->title << endl;
-        } else {
+        } else if (current) {
             cout << "End of playlist!\n";
+        } else {
+            cout << "Playlist is empty.\n";
         }
     }
 
@@ -91,24 +126,95 @@ public:
         if (current && current->prev) {
             current = current->prev;
             cout << "Now playing: " << current->title << endl;
-        } else {
+        } else if (current) {
             cout << "Start of playlist!\n";
+        } else {
+            cout << "Playlist is empty.\n";
         }
+    }
+
+    // Show current song
+    void showCurrent() const {
+        if (current) {
+            cout << "Current song: " << current->title << endl;
+        } else {
+            cout << "No song is currently selected.\n";
+        }
+    }
+
+    // Jump to a specific song
+    void jumpToSong(const string& title) {
+        Song* temp = findSong(title);
+        if (temp) {
+            current = temp;
+            cout << "Jumped to: " << current->title << endl;
+        } else {
+            cout << "Song not found!\n";
+        }
+    }
+
+    // Shuffle play (randomly pick a song)
+    void shufflePlay() {
+        if (size == 0) {
+            cout << "Playlist is empty.\n";
+            return;
+        }
+        vector<Song*> songs;
+        Song* temp = head;
+        while (temp) {
+            songs.push_back(temp);
+            temp = temp->next;
+        }
+        srand(static_cast<unsigned int>(time(nullptr)));
+        int idx = rand() % songs.size();
+        current = songs[idx];
+        cout << "Shuffled to: " << current->title << endl;
+    }
+
+    // Clear the playlist
+    void clear() {
+        Song* temp = head;
+        while (temp) {
+            Song* next = temp->next;
+            delete temp;
+            temp = next;
+        }
+        head = tail = current = nullptr;
+        size = 0;
+    }
+
+    // Show playlist size
+    void showSize() const {
+        cout << "Total songs: " << size << endl;
     }
 };
 
-// Main function
 int main() {
     Playlist myPlaylist;
     int choice;
     string songTitle;
 
     while (true) {
-        cout << "\nMusic Playlist Manager\n";
-        cout << "1. Add Song\n2. Remove Song\n3. Display Playlist Forward\n4. Display Playlist Backward\n";
-        cout << "5. Play Next Song\n6. Play Previous Song\n7. Exit\n";
+        cout << "\n=== Music Playlist Manager ===\n";
+        cout << "1. Add Song\n";
+        cout << "2. Remove Song\n";
+        cout << "3. Display Playlist Forward\n";
+        cout << "4. Display Playlist Backward\n";
+        cout << "5. Play Next Song\n";
+        cout << "6. Play Previous Song\n";
+        cout << "7. Show Current Song\n";
+        cout << "8. Jump to Song\n";
+        cout << "9. Shuffle Play\n";
+        cout << "10. Show Playlist Size\n";
+        cout << "11. Clear Playlist\n";
+        cout << "12. Exit\n";
         cout << "Enter choice: ";
-        cin >> choice;
+        if (!(cin >> choice)) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Invalid input! Please enter a number.\n";
+            continue;
+        }
         cin.ignore();
 
         switch (choice) {
@@ -135,6 +241,24 @@ int main() {
                 myPlaylist.prevSong();
                 break;
             case 7:
+                myPlaylist.showCurrent();
+                break;
+            case 8:
+                cout << "Enter song title to jump to: ";
+                getline(cin, songTitle);
+                myPlaylist.jumpToSong(songTitle);
+                break;
+            case 9:
+                myPlaylist.shufflePlay();
+                break;
+            case 10:
+                myPlaylist.showSize();
+                break;
+            case 11:
+                myPlaylist.clear();
+                cout << "Playlist cleared.\n";
+                break;
+            case 12:
                 cout << "Exiting...\n";
                 return 0;
             default:
